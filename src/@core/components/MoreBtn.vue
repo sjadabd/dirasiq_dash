8025b27bd922cc2f181072172d351c8bd23be971
sjadabd <98034328+sjadabd@users.noreply.github.com>
2025-09-05 15:04:57 +0300

    Initial commit

---
+++ src/@core/components/MoreBtn.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  menuList: {
+    type: Array,
+    required: false,
+  },
+  itemProps: {
+    type: Boolean,
+    required: false,
+  },
+  iconSize: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <IconBtn>
+    <VIcon
+      icon="ri-more-2-line"
+      :size="iconSize"
+    />
+
+    <VMenu
+      v-if="props.menuList"
+      activator="parent"
+    >
+      <VList
+        :items="props.menuList"
+        :item-props="props.itemProps"
+      />
+    </VMenu>
+  </IconBtn>
+</template>
